50e574b4bbcbacc865eac3c2b87fef18b51944a3
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2015-02-05 16:57:19 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var inject = require('gulp-inject');
+var bowerFiles = require('main-bower-files');
+var concat = require('gulp-concat');
+var es = require('event-stream');
+var angularFilesort = require('gulp-angular-filesort');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
   connect.server({
@@ -26,8 +33,67 @@ gulp.task('watch', function () {
 gulp.task('coffee', function() {
   gulp.src('./src/**/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./tmp/coffee/'))
+    .pipe(gulp.dest('./src/tmp/coffee/'))
+});
+
+gulp.task('index', function () {
+  gulp.src('./src/index.html')
+    .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+    .pipe(inject(es.merge(
+      gulp.src(['./src/**/*.js', './src/**/*.css', '!./src/bower/**'], {read: false})
+        .pipe(angularFilesort())
+    )))
+    .pipe(gulp.dest('./src/'));
 });
 
 
-gulp.task('default', ['connect', 'watch']);
+
+gulp.task('ngAnnotate', function () {
+  gulp.src('src/app.js')
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('vendor', function() {
+  gulp.src(bowerFiles({
+      filter: /\.js$/i
+    }))
+    .pipe(uglify())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['./src/**/*.js', '!./src/bower/**'])
+    .pipe(uglify())
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('styles', function() {
+  gulp.src(['./src/**/*.css', '!./src/bower/**'])
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('vendorStyles', function() {
+  gulp.src(bowerFiles({
+      filter: /\.css$/i
+    }))
+    .pipe(concat('bower.css'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('mergeCompiled', function() {
+  gulp.src('./src/index.html')
+    .pipe(inject(gulp.src('./dist/bower.css', {read: false}), {name: 'bower'}))
+    .pipe(inject(gulp.src('./dist/styles.css', {read: false})))
+    .pipe(inject(gulp.src('./dist/vendor.js', {read: false}), {name: 'bower'}))
+    .pipe(inject(gulp.src('./dist/scripts.js', {read: false})))
+    .pipe(gulp.dest('./dist/'))
+});
+
+
+
+gulp.task('default', ['connect', 'index', 'watch']);
+gulp.task('build', ['ngAnnotate', 'styles', 'vendor', 'vendorStyles', 'scripts', 'mergeCompiled']);
