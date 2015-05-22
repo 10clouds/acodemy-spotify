2bb3365e009f9e2883e121ca411b17d419dd31b2
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2015-05-22 13:52:53 +0200

    AngularJS - MVC - 2 - Removed AlbumController - Removed TrackController - Converted everything to ng-repeat over searchResults

--- src/app.js
+++ src/app.js
@@ -1,29 +1,4 @@
 angular.module('app', [])
-.controller('AlbumController', function($scope) {
-  $scope.album = {
-    id: "3B0PgLmgaW0gJth55ApWbw",
-    image: "https://i.scdn.co/image/fa8a3b68578c65fff17f59cc5e5058b4e2fc48b2",
-    name: "Transistor Original Soundtrack"
-  };
-})
-
-.controller('TrackController', function($scope) {
-  $scope.track = {
-    album : {
-      id: "2SmmWWZeC2MXYHpVZgvIHd",
-      name: "Dubai House Collection"
-    },
-    artist: {
-      id: "0wiHUYC8uutA5KUkABFIrT",
-      name: "Parker & Aacht",
-    },
-    duration: "3:30",
-    id: "6gxT7fJ1dAsghuFH6NiRZR",
-    name: "Transistor - Edit",
-    preview_url: "https://p.scdn.co/mp3-preview/f5b0c279a7ba80231ca94285abe0e647d80474f7"
-  };
-})
-
 .controller('NavbarController', function($scope) {
   $scope.search = '';
 })
