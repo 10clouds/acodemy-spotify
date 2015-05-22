477850c517de047b0404f7677495bffcb014173a
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2015-05-22 13:52:53 +0200

    AngularJS - Filters

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 angular.module('app', [])
-.controller('NavbarController', function($scope) {
-  $scope.search = '';
-})
+.filter('duration', function() {
+  return function(durationMs) {
+    var seconds = Math.round(durationMs / 1000);
+    var minutes = Math.floor(seconds / 60);
+    seconds %= 60;
 
+    return minutes + ':' + (seconds < 10 ? '0' + seconds : seconds);
+  };
+})
 .controller('MainController', function($scope) {
+  $scope.search = '';
+
   $scope.searchResults = {
     "albums" : {
       "href" : "https://api.spotify.com/v1/search?query=transistor&offset=0&limit=10&type=album",
