a6a240174a3b747f54c1e49c0465c4e1629a14be
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2015-05-22 14:27:42 +0200

    Apps - Routes - 2 - added album and artist routes

--- src/apis/spotify.js
+++ src/apis/spotify.js
@@ -23,6 +23,70 @@ angular.module('acodemy-app.apis.spotify', [])
         config.params.type = types.join(',');
 
         return $http.get(url, config);
+      },
+
+      getAlbums: function(ids) {
+        var url = this.apiUrl + '/albums';
+        var config = angular.copy(this.defaults);
+        config.params.ids = ids.join(',');
+
+        return $http.get(url, config)
+        .then(function(response) {
+          return response.data.albums;
+        });
+      },
+      getAlbum: function(id) {
+        return this.getAlbums([id])
+        .then(function(albums) {
+          return albums[0];
+        });
+      },
+
+      getArtists: function(ids) {
+        var url = this.apiUrl + '/artists';
+        var config = angular.copy(this.defaults);
+        config.params.ids = ids.join(',');
+
+        return $http.get(url, config)
+        .then(function(response) {
+          return response.data.artists;
+        });
+      },
+      getArtist: function(id) {
+        return this.getArtists([id])
+        .then(function(artists) {
+          return artists[0];
+        });
+      },
+      getArtistsAlbums: function(id) {
+        return $http.get(
+          this.apiUrl + '/artists/' + id + '/albums',
+          this.defaults
+        )
+        .then(function(response) {
+          return response.data;
+        });
+      },
+      getArtistsTopTracks: function(id) {
+        var config = angular.copy(this.defaults);
+        config.params.country = 'US';
+
+        return $http.get(
+          this.apiUrl + '/artists/' + id + '/top-tracks',
+          config
+        )
+        .then(function(response) {
+          return response.data.tracks;
+        });
+      },
+      getRelatedArtists: function(id) {
+        return $http.get(
+          this.apiUrl + '/artists/' + id + '/related-artists',
+          this.defaults
+        )
+        .then(function(response) {
+          return response.data.artists;
+        });
       }
     };
   };
---
+++ src/routes/album/album.js
@@ -0,0 +1,36 @@
+angular.module('acodemy-app.routes.album', [
+  'ngRoute',
+
+  'acodemy-app.apis.spotify',
+  'acodemy-app.filters.duration',
+])
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/album/:id', {
+      templateUrl: 'routes/album/album.html',
+      controller: 'AlbumRouteController'
+    });
+})
+.controller('AlbumRouteController', function(
+  $scope, $routeParams, SpotifyApi
+) {
+  $scope.album = {};
+
+  SpotifyApi.getAlbum($routeParams.id)
+  .then(function(album) {
+    $scope.album = album;
+
+    var artistIds = album.artists.reduce(
+      function(list, artist) {
+        list.push(artist.id);
+        return list;
+      },
+      []
+    );
+
+    return SpotifyApi.getArtists(artistIds);
+  })
+  .then(function(artists) {
+    $scope.artists = artists;
+  });
+});
