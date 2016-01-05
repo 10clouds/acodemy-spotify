f2ba0de32e5338d53ef1ebb2e7e4bedbe87d6925
Wiktor Obrębski <psychowico@gmail.com>
2016-01-05 16:59:03 +0100

    Web Components - AngularJS - 2

--- src/app.js
+++ src/app.js
@@ -54,4 +54,23 @@ angular.module('app', [])
     });
   });
 
+})
+.directive('clearableInput', function() {
+  return {
+    restrict: 'E',
+    require: 'ngModel',
+    link: function ($scope, $element, $attrs, modelCtrl) {
+      var input = $element[0];
+
+      input.addEventListener('input', function() {
+        $scope.$apply(function() {
+          modelCtrl.$setViewValue(input.value);
+        });
+      });
+
+      modelCtrl.$render = function() {
+        input.value = modelCtrl.$viewValue;
+      };
+    }
+  };
 });
