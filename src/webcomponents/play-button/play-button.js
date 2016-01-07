8e8f7426f08ae9815ca7860b60a647d29a30d155
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2016-01-07 15:33:41 +0100

    Web Components - Polymer

--- src/webcomponents/play-button/play-button.js
+++ src/webcomponents/play-button/play-button.js
@@ -1,7 +1,4 @@
 (function() {
-  const ownerDocument = document._currentScript.ownerDocument;
-  const template = ownerDocument.querySelector('template');
-
   const states = {
     stopped: {
       iconClass: 'fa fa-play',
@@ -17,95 +14,73 @@
     }
   };
 
-  class PlayButton extends HTMLElement {
-    get src() {
-      const attr = this.attributes.getNamedItem('src');
-      return attr ? attr.value : '';
-    }
+  const players = [];
 
-    set src(value) {
-      let attr = this.attributes.getNamedItem('src');
-      if (!attr) {
-        attr = document.createAttribute('src');
-        this.attributes.setNamedItem(attr);
-      }
-      attr.value = value;
-    }
+  Polymer({
+    is: 'play-button',
+    properties: {
+      src: {
+        type: String,
+        notify: true,
+        observer: 'srcChanged',
+      },
+      state: {
+        type: String,
+        observer: 'stateChanged',
+        default: 'stopped',
+      },
+      _iconClass: String,
+    },
 
-    get state() {
-      return this._stateName || 'stopped';
-    }
+    stateChanged: function(stateName) {
+      this._iconClass = states[stateName].iconClass;
+    },
 
-    set state(name) {
-      this._stateName = name;
-      this.icon.className = states[name].iconClass;
-    }
+    srcChanged: function() {
+      this.stop();
+    },
 
-    createdCallback() {
-      const shadow = this.createShadowRoot();
-      const clone = document.importNode(template.content, true);
-      shadow.appendChild(clone);
-      const button = shadow.querySelector('button');
+    onClick: function() {
+      const nextMethod = states[this.state].next;
+      this[nextMethod]();
+    },
 
-      this.icon = button.querySelector('button i');
-      this.audio = shadow.querySelector('audio');
+    attached: function() {
+      players.push(this);
+    },
 
-      this.state = 'stopped';
-
-      button.addEventListener('click', (function() {
-        const nextMethod = states[this.state].next;
-        this[nextMethod]();
-      }).bind(this));
-    };
-
-    attachedCallback() {
-      PlayButton.list.push(this);
-    };
-
-    detachedCallback() {
-      const index = PlayButton.list.indexOf(this);
+    detached: function() {
+      const index = players.indexOf(this);
 
       if (index >= 0) {
-        PlayButton.list.splice(index, 1);
+        players.splice(index, 1);
       }
-    };
+    },
 
-    attributeChangedCallback(attrName, oldValue, newValue) {
-      if (attrName === 'src') {
-        this.stop();
-      }
-    };
-
-    play() {
-      PlayButton
-      .list
+    play: function() {
+      players
       .filter((other) => other !== this)
       .forEach((other) => other.stop());
 
-      if (this.src !== this.audio.src) {
-        this.audio.src = this.src;
-        this.audio.load();
+      if (this.src !== this.$.audio.src) {
+        this.$.audio.src = this.src;
+        this.$.audio.load();
       }
 
-      this.audio.play();
+      this.$.audio.play();
       this.state = 'playing';
-    };
+    },
 
-    pause() {
-      this.audio.pause();
+    pause: function() {
+      this.$.audio.pause();
       this.state = 'paused';
-    };
+    },
 
-    stop() {
-      this.audio.pause();
-      this.audio.currentTime = 0;
+    stop: function() {
+      this.$.audio.pause();
+      this.$.audio.currentTime = 0;
       this.state = 'stopped';
-    };
-  }
-
-  PlayButton.list = [];
-
-
-  document.registerElement('play-button', PlayButton);
+    }
+  });
 
 })();
