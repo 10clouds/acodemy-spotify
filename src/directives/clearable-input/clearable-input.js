9be2571b7ed675f070ed56dbe45977cb4d33f2d3
Marek Skrajnowski <marek.skrajnowski@10clouds.com>
2016-01-07 15:33:41 +0100

    Web Components - AngularJS - 2 - added the clearable-input component wrapper

---
+++ src/directives/clearable-input/clearable-input.js
@@ -0,0 +1,17 @@
+angular.module('app')
+  .directive('clearableInput', function() {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      link: function($scope, $element, $attrs, modelCtrl) {
+        const input = $element[0];
+
+        input.addEventListener('input', () =>
+          $scope.$apply(() => modelCtrl.$setViewValue(input.value))
+        );
+
+        modelCtrl.$render = () => input.value = modelCtrl.$viewValue;
+      }
+    };
+  });
+
